4d43f38c31dcd56c4b5e9ff5ea4830f1939c93f6
JeweIxx <vhiunese@gmail.com>
2022-12-08 18:44:43 +0400

    added styling warning feature which lets the user know of a certain bug, weather this should be shown gets stored in the sessionstorage

---
+++ src/components/Utility/StylingWarning.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function stylingWarning(props) {
+  const [closeWarning, setCloseWarning] = useState(false);
+
+  return (
+    <>
+      {!sessionStorage.getItem("firstTimeRefresh") ? (
+        <div
+          className={`absolute ${
+            closeWarning ? "sm:hidden" : ""
+          }top-0  z-20  justify-center p-8 hidden sm:flex items-center left-0 w-full overflow-y-hidden h-full bg-gradient-to-b from-black to-gray-800 text-gray-500`}
+        >
+          <p
+            onClick={() => {
+              // session storage is used to know if the user has opened up the site for the first time
+              // but it doesnt last forever unlike localstorage which lingers even beyond the site closing
+              //this is perfect for popups which u need to show only when the user opens up the page,
+              //but no on every re-render of the component.
+              sessionStorage.setItem("firstTimeRefresh", true);
+              setCloseWarning(true);
+            }}
+            className="text-4xl absolute top-0 right-0 mr-10 mt-10  hover:scale-110 cursor-pointer transition duration-300 active:scale-90"
+          >
+            x
+          </p>
+
+          <div className="flex flex-col justify-center items-center text-center">
+            <p className="text-2xl text-white">
+              Sayt qəribə görsəndiyi halda ctrl və - düymələrinə basın.{" "}
+            </p>
+            <br />
+
+            <p className="text-2xl text-white">
+              In the case of the site looking rather weird, press the ctrl and -
+              keys.
+            </p>
+            <br />
+            <br />
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+}
+
+export default stylingWarning;
